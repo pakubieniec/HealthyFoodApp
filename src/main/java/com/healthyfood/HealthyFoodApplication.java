8bfb24b1afea6382d275421912189a7cf6ffdcfb
pakubieniec <pakubieniec@gmail.com>
2024-03-05 15:10:59 +0100

    Fixes in connection with API. Additionally, checking the status codes. Additional tests.

--- src/main/java/com/healthyfood/HealthyFoodApplication.java
+++ src/main/java/com/healthyfood/HealthyFoodApplication.java
@@ -4,14 +4,18 @@ import com.healthyfood.connectivity.FetchApiFood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.ConnectException;
+
 @SpringBootApplication
 public class HealthyFoodApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ConnectException {
 
         SpringApplication.run(HealthyFoodApplication.class, args);
         FetchApiFood fetch = new FetchApiFood();
-        System.out.print(fetch.getProductByName("Chicken"));
+//        System.out.print(fetch.getProductByName("coffee"));
+//        System.out.println(fetch.getAllProducts());
+//        System.out.println(fetch.getProductByFoodId("food_bmyxrshbfao9s1amjrvhoauob6mo"));
 //        System.out.print(fetch.getProductByFoodId("food_bq6stkiaxkwhxia9q4v7wanjnew0"));
     }
 }
--- src/main/java/com/healthyfood/connectivity/FetchApiFood.java
+++ src/main/java/com/healthyfood/connectivity/FetchApiFood.java
@@ -9,52 +9,64 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class FetchApiFood {
     private final HttpClient client = HttpClient.newBuilder().build();
+    private final String FOOD_API_KEY = "https://api.edamam.com/api/food-database/v2/parser?app_id=a5c1a6e2&app_key=%20917d9904d1aa8302eb4d353787034eb6&nutrition-type=cooking&ingr=";
 
     public String getProductByName(String productName) {
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.edamam.com/api/food-database/v2/parser?app_id=a5c1a6e2&app_key=%20917d9904d1aa8302eb4d353787034eb6&ingr=" + CapitalizeTheFirstLetter.capitalizeTheFirstLetter(productName) + "&nutrition-type=cooking")).GET().build();
-            return getResponds(request);
-        } catch (Exception e) {
-            e.printStackTrace();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(FOOD_API_KEY + CapitalizeTheFirstLetter.capitalizeTheFirstLetter(productName))).GET().build();
+            return checkStatusCodeResponse(request);
+        } catch (NullPointerException e) {
+            e.getMessage();
         }
         return null;
     }
 
     public String getProductByFoodId(String foodId) {
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.edamam.com/api/food-database/v2/parser?app_id=a5c1a6e2&app_key=%20917d9904d1aa8302eb4d353787034eb6&ingr=" + foodId + "&nutrition-type=cooking")).GET().build();
-            return getResponds(request);
-        } catch (Exception e) {
-            e.printStackTrace();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(FOOD_API_KEY + foodId)).GET().build();
+            return checkStatusCodeResponse(request);
+        } catch (NullPointerException e) {
+            e.getMessage();
         }
         return null;
     }
 
     public List<String> getAllProducts() {
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.edamam.com/api/food-database/v2/parser?app_id=a5c1a6e2&app_key=%20917d9904d1aa8302eb4d353787034eb6")).GET().build();
-            return Collections.singletonList(getResponds(request));
-        } catch (Exception e) {
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(FOOD_API_KEY)).GET().build();
+            return Collections.singletonList(checkStatusCodeResponse(request));
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+    private HttpResponse<String> getResponse(HttpRequest request) {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private String getResponds(HttpRequest request) {
-        try {
-            HttpResponse<String> response;
-            try {
-                response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            return response.body();
-        } catch (Exception e) {
-            e.printStackTrace();
+private String checkStatusCodeResponse(HttpRequest request) {
+        int statusCode = Objects.requireNonNull(getResponse(request)).statusCode();
+        if (statusCode == 200) {
+            return getResponse(request).body();
+        } else if (statusCode >= 400 && statusCode <= 499) {
+            return "The specified URL was not found or couldn't be retrieved";
+        } else if (statusCode >= 500 && statusCode <= 599) {
+            return "Server error responses";
+        } else {
+            return String.valueOf(statusCode);
         }
-        return null;
     }
+
 }
+
+
